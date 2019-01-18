c6df491468ba7c6b7ba5872b91a3e33f1dddeacd
LinnierGames <esericksanc@gmail.com>
2019-01-18 10:36:54 -0800

    Relink Source Files

---
+++ UIPickers/Classes/UISearchTablePickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UISearchTablePickerViewController.swift
+//  FBSnapshotTestCase
+//
+//  Created by Erick Sanchez on 1/18/19.
+//
+
+import UIKit
+
+class UISearchTablePickerViewController: UITablePickerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
