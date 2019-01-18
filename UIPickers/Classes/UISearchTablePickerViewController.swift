d2e0b015c329714446fabf0f5c47f588ac0520c6
LinnierGames <esericksanc@gmail.com>
2019-01-18 11:01:56 -0800

    WIP: UISearchTableVc
    TODO: Test it all

--- UIPickers/Classes/UISearchTablePickerViewController.swift
+++ UIPickers/Classes/UISearchTablePickerViewController.swift
@@ -7,23 +7,87 @@
 
 import UIKit
 
+protocol UISearchTablePickerViewControllerDataSource: class {
+    
+    func numberOfSections(in tableView: UITableView, searchTerm: String?) -> Int
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int, searchTerm: String?) -> Int
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, searchTerm: String?) -> UITableViewCell
+}
+
 class UISearchTablePickerViewController: UITablePickerViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - VARS
+    
+    public weak var dataSource: UISearchTablePickerViewControllerDataSource?
+    
+    public private(set) var searchBar: UISearchBar = {
+        let sb: UISearchBar = UISearchBar.initProgrammatically()
+        
+        return sb
+    }()
+    
+    // MARK: - RETURN VALUES
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        guard let dataSource = self.dataSource else {
+            return super.numberOfSections(in: tableView)
+        }
+        
+        let term: String? = {
+            if let text = self.searchBar.text, text.isEmpty == false {
+                return text
+            } else {
+                return nil
+            }
+        }()
+        
+        return dataSource.numberOfSections(in: tableView, searchTerm: term)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let dataSource = self.dataSource else {
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
+        
+        let term: String? = {
+            if let text = self.searchBar.text, text.isEmpty == false {
+                return text
+            } else {
+                return nil
+            }
+        }()
+        
+        return dataSource.tableView(tableView, numberOfRowsInSection: section, searchTerm: term)
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let dataSource = self.dataSource else {
+            return super.tableView(tableView, cellForRowAt: indexPath)
+        }
+        
+        let term: String? = {
+            if let text = self.searchBar.text, text.isEmpty == false {
+                return text
+            } else {
+                return nil
+            }
+        }()
+        
+        return dataSource.tableView(tableView, cellForRowAt: indexPath, searchTerm: term)
+    }
+    
+    override func layoutContent() -> [UIView] {
+        
+        // this contains the table view
+        let superContent = super.layoutContent()
+        
+        return [searchBar] + superContent
+    }
+    
+    // MARK: - METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
 
 }
